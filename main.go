52b4c223cbe437d0cd8ea409aa77047fe5ad3473
Kieran <kieran.webdevelopment@gmail.com>
2023-12-02 15:21:52 +0000

    create repl.go file

--- main.go
+++ main.go
@@ -1,65 +1,69 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-	"strings"
+    // "bufio"
+    // "fmt"
+    // "os"
+	// "strings"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
+// type cliCommand struct {
+// 	name        string
+// 	description string
+// 	callback    func() error
+// }
 
-var commands = map[string]cliCommand{
-	"help": {
-        name:        "help",
-        description: "Displays a help message",
-        callback:    commandHelp,
-    },
-    "exit": {
-        name:        "exit",
-        description: "Exit the Pokedex",
-        callback:    commandExit,
-    },
-}
+// var commands = map[string]cliCommand{
+// 	"help": {
+//         name:        "help",
+//         description: "Displays a help message",
+//         callback:    commandHelp,
+//     },
+//     "exit": {
+//         name:        "exit",
+//         description: "Exit the Pokedex",
+//         callback:    commandExit,
+//     },
+// }
 
 func main() {
-    reader := bufio.NewReader(os.Stdin)
+//     scanner := bufio.NewScanner(os.Stdin)
+//     scanner.Scan()
+//     text := scanner.Text()
+// fmt.Println(text)
+startRepl()
 
-    for {
-        fmt.Print("Pokedex > ")
-        text, _ := reader.ReadString('\n')
-        input := strings.TrimSpace(text)
+    // for {
+    //     fmt.Print("Pokedex > ")
+    //     text, _ := reader.ReadString('\n')
+    //     input := strings.TrimSpace(text)
         
-        cmd, ok := commands[input]
-        if !ok {
-            fmt.Println("Unknown command")
-            continue
-        }
+    //     cmd, ok := commands[input]
+    //     if !ok {
+    //         fmt.Println("Unknown command")
+    //         continue
+    //     }
         
-       cmd.callback()
+    //    cmd.callback()
 		
-        if input == "exit" {
-            break
-        }
-    }
+    //     if input == "exit" {
+    //         break
+    //     }
+    // }
 }
 
-func commandHelp() error {
-    fmt.Println(`
-        Welcome to the Pokedex!
-        Usage:
+// func commandHelp() error {
+//     fmt.Println(`
+//         Welcome to the Pokedex!
+//         Usage:
         
-        help: Displays a help message
-        exit: Exits the program
-    `)
-    return nil
-}
+//         help: Displays a help message
+//         exit: Exits the program
+//     `)
+//     return nil
+// }
 
-func commandExit() error {
-    fmt.Println("Exiting")
-    return nil 
-}
+// func commandExit() error {
+//     fmt.Println("Exiting")
+//     return nil 
+// }
