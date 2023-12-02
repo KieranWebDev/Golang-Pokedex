0a5a8e8b134da68574c1777e49bcb3477eebf58c
Kieran <kieran.webdevelopment@gmail.com>
2023-12-02 16:48:10 +0000

    added command_Help file and getCommands function

--- repl.go
+++ repl.go
@@ -1,25 +1,64 @@
-package main 
+package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
-func startRepl(){
+func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-	fmt.Print(">")
-    scanner.Scan()
-    text := scanner.Text()
-	fmt.Println(text)
+		fmt.Print(">")
+
+		scanner.Scan()
+		text := scanner.Text()
+		cleanedText := cleanInput(text)
+
+		if len(cleanedText) == 0 {
+			continue
+		}
+
+		command := cleanedText[0]
+
+		switch command {
+		case "help":
+			fmt.Println("this is the help menu")
+		case "exit":
+			os.Exit(0)
+		default:
+			fmt.Println("invalid command")
+		}
+
+		// fmt.Println("echo", command)
 
 	}
 }
 
-func cleanInput(str string) []string{
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func cleanInput(str string) []string {
 	lowerCase := strings.ToLower(str)
 	words := strings.Fields(lowerCase)
 	return words
